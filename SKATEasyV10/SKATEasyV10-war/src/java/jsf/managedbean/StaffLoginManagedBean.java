682038494868aad8b004dbe12bf02437621d56a9
kelpal <67057261+kelpal@users.noreply.github.com>
2022-04-03 12:21:55 +0800

    JSF (StaffLoginManagedBean, filter and listener)

---
+++ SKATEasyV10/SKATEasyV10-war/src/java/jsf/managedbean/StaffLoginManagedBean.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import ejb.session.stateless.StaffEntitySessionBeanLocal;
+import entity.StaffEntity;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.faces.event.ActionEvent;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import util.exception.InvalidLoginCredentialException;
+
+/**
+ *
+ * @author kel
+ */
+
+@Named(value = "staffLoginManagedBean")
+@RequestScoped
+public class StaffLoginManagedBean implements Serializable {
+
+    @EJB
+    private StaffEntitySessionBeanLocal staffEntitySessionBeanLocal;
+    
+    private String username;
+    private String password;
+    
+    
+    public StaffLoginManagedBean() {
+    }
+    
+        
+    public void login(ActionEvent event) throws IOException {
+        try {
+            StaffEntity currentStaff = staffEntitySessionBeanLocal.staffLogin(username, password);
+            FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isLogin", true);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("currentStaff", currentStaff);
+            FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+        } catch (InvalidLoginCredentialException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid login credential: " + ex.getMessage(), null));
+        }
+    }
+
+    public void logout(ActionEvent event) throws IOException {
+        ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).invalidate();
+        FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
